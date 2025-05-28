c8d6c1e019369500a067ce4a5ce731ceb0b0d079
ThePeoplesPlaza <placegeorgetown@gmail.com>
2025-05-28 09:35:07 -0700

    Add files via upload

--- unit-script.js
+++ unit-script.js
@@ -44,10 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
       delay: 4000,
       disableOnInteraction: false,
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
