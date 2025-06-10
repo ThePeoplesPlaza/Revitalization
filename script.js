c11afd85b62ef265b103b8ec60ec15257cd59212
ThePeoplesPlaza <placegeorgetown@gmail.com>
2025-06-10 11:11:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const units = [
     { title: "A1", price: 1700, viewLink: "unit-a1.html", applyLink: "https://docs.google.com/forms/d/e/1FAIpQLSeFgBE2i5baAcGrmPqayblZnQNrXJPpLK30LYcIPcEWDMPm-Q/viewform?usp=dialog" },
     { title: "A7", price: 2700, viewLink: "unit-a7.html"},
-    { title: "B12", price: 770, viewLink: "https://showmojo.com/l/7433951017/4545-georgetown-pl-b12-stockton-ca-95207?g=2&sd=true"},
+    { title: "B12", price: 770, viewLink: "unit-b12.html"},
     { title: "C34", price: 1265, viewLink: "https://showmojo.com/l/74dfacf017/4545-georgetown-pl-c34-stockton-ca-95207?fullscreen=true&g=2&sd=true"},
     { title: "C35", price: 1210, viewLink: "https://showmojo.com/l/752cfd3017/4545-georgetown-pl-c35-stockton-ca-95207?fullscreen=true&g=2&sd=true"},
     { title: "C36", price: 1100, viewLink: "https://showmojo.com/l/7577298017/4545-georgetown-pl-c36-stockton-ca-95207?fullscreen=true&g=2&sd=true"},
